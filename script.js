f114fefb1c8ee09660a51143dfdc28c8068d7dae
AndyKodehode <andykodehode@gmail.com>
2024-10-30 10:24:39 +0100

    oppdatert design og instrukser

--- script.js
+++ script.js
@@ -5,4 +5,30 @@
 
 //Du må lage en eventlistener som er koblet til knappen
 
-// Du må lage et nytt element når du trykker på knappen
+// NÅR DU TRYKKER PÅ KNAPPEN//
+
+/*
+
+- Du må lage en ny div
+- Den nye div må ha styling til kanbanCard
+- Det nye kortet må legges til i cardContainer
+- Du må lage en ny h3 og legge den til i kortet
+- du må legge til en text på h3
+- du må legge til styling for h3
+
+*/
+
+
+// METODER SOM KAN BRUKES//
+
+/* 
+- queryselector
+- createElement
+- classist.add
+- appendChild
+- prepend (som appendChild, men legger ting i start av array)
+- textContent
+
+*/
+
+
