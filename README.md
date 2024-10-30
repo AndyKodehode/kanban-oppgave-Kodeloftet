119960b74fffe32e51f2a5e6a944dff1edb36c0f
AndyKodehode <andykodehode@gmail.com>
2024-10-30 08:58:26 +0100

    js kommentarer

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # kanban-oppgave-Kodeloftet
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/AndyKodehode/kanban-oppgave-Kodeloftet)
--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello!');
+
+// Lag en funksjon som lager et nytt kort når man trykker på "Add card +"
+
+// Du må hente inn de forskjellige elementene fra HTML
+
+//Du må lage en eventlistener som er koblet til knappen
+
+// Du må lage et nytt element når du trykker på knappen
